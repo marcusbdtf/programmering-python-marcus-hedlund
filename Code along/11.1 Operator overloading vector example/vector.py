7bdc7b986da6002d3c05112a65d888a2a61d4e85
marcusbdtf <marcus.hedlund@hotmail.com>
2021-10-06 20:50:14 +0200

    Vector Update

---
+++ Code along/11.1 Operator overloading vector example/vector.py
@@ -0,0 +1,49 @@
+from typing import Type
+
+
+class Vector:
+    """A class to represant a Euclidean vector with magnitute and direction"""
+
+    def __init__(self, *numbers: float) -> None: # star means positional meter
+        #print(numbers)
+        for number in numbers:
+            if not isinstance(number, (float,int)):
+                raise TypeError(f"{number} is must be float or int")
+
+        if len(numbers) <= 0:
+            raise ValueError("Vectors can't be empty")
+
+        self._numbers = tuple(float(number) for number in numbers)
+
+    @property
+    def numbers(self) -> tuple:
+        return self._numbers
+
+    # (2,3) + (1,1,1) is not okay
+    # (2,3) + (1,1) = (3,4) is okay
+    def __add__(self, other: "Vector") -> "Vector":
+        if self.validate_vectors(other):
+            numbers = (a+b for a,b in zip(self.numbers, other.numbers))
+            return Vector(*numbers)
+    # len() function
+    def __len__(self) -> int:
+        """ returns number of components in a Vector not the Euclidean length"""
+        return len(self.numbers)
+           
+
+    def validate_vectors(self, other: "Vector") -> bool:
+        """ validates that two vectors have same dimentions """
+        if not isinstance(other, Vector) or len(other) != len(self):
+            raise TypeError("Both must be Vector and same length")
+        return len(self) == len(other)
+
+    def __repr__(self) -> str:
+        return f"Vector{self.numbers}"
+
+    # [] operator
+    def __getitem__(self, item: int) -> float:
+        return self.numbers[item]
+
+
+
+# v1 = (1,1), v2 = (1,1,14,15,252,56,2,7) (examples)
